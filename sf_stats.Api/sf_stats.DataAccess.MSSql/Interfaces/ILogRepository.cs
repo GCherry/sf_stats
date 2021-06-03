906699aead8fa284585cf21df930f5dd126dfcb6
gcherry <gcherry2@gmail.com>
2021-06-03 01:48:12 -0400

    empty repo + interface

---
+++ sf_stats.Api/sf_stats.DataAccess.MSSql/Interfaces/ILogRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sf_stats.DataAccess.MSSql.Interfaces
+{
+    public interface ILogRepository
+    {
+    }
+}
---
+++ sf_stats.Api/sf_stats.DataAccess.MSSql/Repositories/LogRepository.cs
@@ -0,0 +1,13 @@
+﻿using sf_stats.DataAccess.MSSql.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sf_stats.DataAccess.MSSql.Repositories
+{
+    public class LogRepository : ILogRepository
+    {
+    }
+}
