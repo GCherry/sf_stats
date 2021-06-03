7b27d6511f4d84d5ec672728a55c2f817186a98f
Greg Cherry <Gcherry2@gmail.com>
2021-06-03 01:25:11 -0400

    Initial DB migration (#1)

---
+++ sf_stats.Api/sf_stats.DataAccess.MSSql/Context/SFStatDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sf_stats.DataAccess.MSSql.EntityConfigurations;
+using sf_stats.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sf_stats.DataAccess.MSSql.Context
+{
+    public class SFStatDbContext : DbContext
+    {
+        public SFStatDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Log> Log { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("dbo");
+
+            modelBuilder.ApplyConfiguration(new LogEntityConfiguration());
+        }
+    }
+}
