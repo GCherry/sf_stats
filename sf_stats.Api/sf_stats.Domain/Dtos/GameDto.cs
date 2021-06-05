45c43cce202689233d1369b11a1d49855bd6cc7d
joshua <mail@joshuatland.com>
2021-06-05 01:29:42 -0400

    removed data annotations

--- sf_stats.Api/sf_stats.Domain/Dtos/GameDto.cs
+++ sf_stats.Api/sf_stats.Domain/Dtos/GameDto.cs
@@ -11,17 +11,9 @@ namespace sf_stats.Domain.Dtos
     public class GameDto
     {
         public int Id { get; set; }
-
-        [ForeignKey("Season")]
         public int SeasonId { get; set; }
-
-        [ForeignKey("Division")]
         public int DivisionId { get; set; }
-
-        [ForeignKey("Team")]
         public int Home_TeamId { get; set; }
-
-        [ForeignKey("Team")]
         public int Away_TeamId { get; set; }
 
         public virtual Season Season { get; set; }
