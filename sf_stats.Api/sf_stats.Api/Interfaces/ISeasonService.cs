6a20cd3365b0bd87ab4440039f93d820951b2bef
gcherry <gcherry2@gmail.com>
2021-06-11 01:50:01 -0400

    add season service and repository

---
+++ sf_stats.Api/sf_stats.Api/Interfaces/ISeasonService.cs
@@ -0,0 +1,17 @@
+﻿using sf_stats.Domain.DomainObjects;
+using sf_stats.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace sf_stats.Api.Interfaces
+{
+    public interface ISeasonService
+    {
+        Task<Season> AddAsync(Season season);
+        Task DeleteByIdAsync(int seasonId);
+        Task<List<Season>> GetAsync(SeasonQueryFilter filter);
+        Task<Season> GetAsync(int seasonId);
+        Task SaveChangesAsync();
+        Season Update(Season season);
+    }
+}
---
+++ sf_stats.Api/sf_stats.Api/Services/SeasonService.cs
@@ -0,0 +1,49 @@
+﻿using sf_stats.Api.Interfaces;
+using sf_stats.DataAccess.MSSql.Interfaces;
+using sf_stats.Domain.DomainObjects;
+using sf_stats.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace sf_stats.Api.Services
+{
+    public class SeasonService : ISeasonService
+    {
+        private readonly ISeasonRepository _seasonRepository;
+
+        public SeasonService(ISeasonRepository seasonRepository)
+        {
+            _seasonRepository = seasonRepository;
+        }
+
+        public async Task<Season> AddAsync(Season season)
+        {
+            return await _seasonRepository.AddAsync(season);
+        }
+
+        public async Task DeleteByIdAsync(int seasonId)
+        {
+            await _seasonRepository.DeleteByIdAsync(seasonId);
+        }
+
+        public async Task<List<Season>> GetAsync(SeasonQueryFilter filter)
+        {
+            return await _seasonRepository.GetAsync(filter);
+        }
+
+        public async Task<Season> GetAsync(int seasonId)
+        {
+            return await _seasonRepository.GetAsync(seasonId);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _seasonRepository.SaveChangesAsync();
+        }
+
+        public Season Update(Season season)
+        {
+            return _seasonRepository.Update(season);
+        }
+    }
+}
