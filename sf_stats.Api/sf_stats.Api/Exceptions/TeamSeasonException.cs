ee5a26159957234526efd5ac6278f63e1489bbbe
Joshua Land <mail@joshuatland.com>
2022-05-28 15:52:15 -0400

    added teamseason exception object

---
+++ sf_stats.Api/sf_stats.Api/Exceptions/TeamSeasonException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sf_stats.Api.Exceptions
+{
+    public class TeamSeasonException : SFStatsExceptionBase
+    {
+        public TeamSeasonException() : base()
+        {
+        }
+
+        public TeamSeasonException(string message) : base(message)
+        {
+        }
+
+        public TeamSeasonException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
